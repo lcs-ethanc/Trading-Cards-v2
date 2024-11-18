d362fa869243d988ea84ce48cb6247d42c40a2f7
Ethan Chen <echen@lcs.on.ca>
2024-11-18 13:46:51 -0500

    Initial Commit

---
+++ Trading Cards v2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trading Cards v2
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trading Cards v2/Trading_Cards_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Trading_Cards_v2App.swift
+//  Trading Cards v2
+//
+//  Created by Pak Ching Ethan Chen on 2024-11-18.
+//
+
+import SwiftUI
+
+@main
+struct Trading_Cards_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
